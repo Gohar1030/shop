7ae47c7759d4175a73e03002a49005c8dc42ab2e
Gohar <gohar@macs-MacBook-Pro.local>
2018-10-14 14:02:21 +0400

    shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavInfoComponent } from './nav-info/nav-info.component';
+import { MainComponent } from './main/main.component';
+import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavInfoComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatButtonModule,
+
+    RouterModule.forRoot(
+      [
+        {path: '', pathMatch:'full', redirectTo:'home'},
+        {path:'home', component:MainComponent}
+      ]
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
